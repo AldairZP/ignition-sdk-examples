bcdbe65ff87b9b19d8191a62591a44baaf7b7ce4
Jonathan Zapata Ponciano <JONZAPA@tbdir.net>
2026-01-08 11:51:47 -0600

    Enhance Messenger and Plane components with new properties and improved functionality

--- perspective-component/web/packages/client/typescript/components/Messenger.tsx
+++ perspective-component/web/packages/client/typescript/components/Messenger.tsx
@@ -26,43 +26,67 @@ export const COMPONENT_TYPE = "rad.display.messenger";
  */
 export const MESSAGE_CONFIG_PROP = "messageConfig";
 
-interface MessengerPropsString {
-  points: string;
-}
-// interface MessengerProps {
-//   points: PointData[];
+// interface MessengerPropsString {
+//   points: string;
 // }
+interface MessengerProps {
+  points: PointData[];
+  urlImage: string;
+  widthImage: number;
+  heightImage: number;
+  background: string;
+  colorLine: string;
+  subdivisions: number;
+  buttons: boolean;
+  zoomValue: number;
+  colorPoints: string;
+}
 
 // Default configuration in component props. Added here just as a useful reference.
-export const DEFAULT_MESSAGE_CONFIG: PointData[] = [{ id: "0", x: 1, y: 2 }];
+// export const DEFAULT_MESSAGE_CONFIG: PointData[] = [{ id: "0", x: 1, y: 2 }];
+export const DEFAULT_MESSAGE_CONFIG: PointData[] = [];
 /**
  * Our Perspective component, written as a React functional component.
  */
 const onPointsChange = (
-  props: ComponentProps<MessengerPropsString>,
-  newPoints: string
+  props: ComponentProps<MessengerProps>,
+  newPoints: PointData[]
 ) => {
   props.store.props.write("points", newPoints);
 };
 
-export const MessengerComponent = (
-  props: ComponentProps<MessengerPropsString>
-) => {
-  console.log(props.props.points);
-  const messageProps = JSON.parse(props.props.points);
+export const MessengerComponent = (props: ComponentProps<MessengerProps>) => {
+  // console.log(props.props.points);
+  // const messageProps = JSON.parse(props.props.points);
   console.log(props.props);
+  //  {...this.props.emit({ classes: ["messenger-component"] })}
   return (
-    <Plane
-      points={messageProps}
-      onPointsChange={(newPoints) => {
-        onPointsChange(props, JSON.stringify(newPoints));
-      }}
-    />
+    <div {...props.emit({ classes: ["messenger-component"] })}>
+      <Plane
+        points={props.props.points}
+        onPointsChange={(newPoints) => {
+          onPointsChange(props, newPoints);
+        }}
+        urlImage={props.props.urlImage}
+        widthImage={props.props.widthImage}
+        heightImage={props.props.heightImage}
+        background={props.props.background}
+        colorLine={props.props.colorLine}
+        subdivisions={props.props.subdivisions}
+        buttons={props.props.buttons}
+        zoomValue={props.props.zoomValue}
+        colorPoints={props.props.colorPoints}
+      />
+    </div>
   );
 };
 
 // This is the actual thing that gets registered with the component registry.
 export class MessengerComponentMeta implements ComponentMeta {
+  isContainer = true;
+  isDeepSelectable = true;
+  focusRootOnly = true;
+
   getComponentType(): string {
     return COMPONENT_TYPE;
   }
@@ -79,9 +103,18 @@ export class MessengerComponentMeta implements ComponentMeta {
     return MessengerComponent;
   }
 
-  getPropsReducer(tree: PropertyTree): MessengerPropsString {
+  getPropsReducer(tree: PropertyTree): MessengerProps {
     return {
-      points: tree.read("points"),
+      points: tree.read("points", DEFAULT_MESSAGE_CONFIG),
+      urlImage: tree.read("urlImage", undefined),
+      widthImage: tree.read("widthImage", 0),
+      heightImage: tree.read("heightImage", 0),
+      background: tree.read("background", "#000"),
+      colorLine: tree.read("colorLine", "#333"),
+      subdivisions: tree.read("subdivisions", 1),
+      buttons: tree.read("buttons", false),
+      zoomValue: tree.read("zoom", 3),
+      colorPoints: tree.read("colorPoints", "#fff"),
     };
   }
 }
--- perspective-component/web/packages/client/typescript/components/Plane/Plane.tsx
+++ perspective-component/web/packages/client/typescript/components/Plane/Plane.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Mafs, Coordinates, Point } from "mafs";
 
 import { MODES, type Mode, type PointData } from "./types";
@@ -6,6 +6,7 @@ import { EditablePoint } from "./EditablePoint";
 import { DeletablePoint } from "./DeletablePoint";
 import { ModeSelector } from "./ModeSelector";
 import { usePoints } from "./hooks/usePoints";
+import { Image } from "mafs";
 
 export interface PlaneProps {
   /** Puntos iniciales (solo se usa en modo no controlado) */
@@ -14,14 +15,33 @@ export interface PlaneProps {
   points?: PointData[];
   /** Callback cuando los puntos cambian (para modo controlado) */
   onPointsChange?: (points: PointData[]) => void;
+  urlImage?: string;
+  widthImage?: number;
+  heightImage?: number;
+  background?: string;
+  colorLine?: string;
+  subdivisions?: number;
+  buttons?: boolean;
+  zoomValue?: number;
+  colorPoints?: string;
 }
 
 export function Plane({
   initialPoints,
   points,
   onPointsChange,
+  urlImage,
+  widthImage,
+  heightImage,
+  background,
+  colorLine,
+  subdivisions = 1,
+  buttons = false,
+  zoomValue = 3,
+  colorPoints,
 }: PlaneProps = {}) {
   const [mode, setMode] = useState<Mode>(MODES.VIEW);
+  const [containerHeight, setContainerHeight] = useState<number | undefined>(0);
   const {
     points: currentPoints,
     createPoint,
@@ -34,29 +54,64 @@ export function Plane({
     const [x, y] = point;
     createPoint(x, y);
   }
-
+  useEffect(() => {
+    const el = document.getElementById("container");
+    const newContainerHeight = el?.getBoundingClientRect().height;
+    setContainerHeight(newContainerHeight);
+  }, []);
+  console.log(containerHeight);
   return (
-    <>
-      <ModeSelector currentMode={mode} onModeChange={setMode} />
-      {/* <ModeInstructions mode={mode} /> */}
+    <div className="container">
+      {buttons && <ModeSelector currentMode={mode} onModeChange={setMode} />}
 
-      <Mafs onClick={(point: [number, number]) => handleMafsClick(point)}>
-        <Coordinates.Cartesian />
+      <div id="container">
+        <Mafs
+          zoom={{ min: 0.2, max: 10 }}
+          height={containerHeight}
+          width={"auto"}
+          viewBox={{ x: [-zoomValue, zoomValue], y: [-zoomValue, zoomValue] }}
+          onClick={(point: [number, number]) => handleMafsClick(point)}
+        >
+          <Coordinates.Cartesian
+            subdivisions={subdivisions}
+            xAxis={subdivisions > 0 ? { axis: false, labels: false } : false}
+            yAxis={subdivisions > 0 ? { axis: false, labels: false } : false}
+          />
+          <Image
+            href={urlImage}
+            width={widthImage}
+            height={heightImage}
+            anchor="cc"
+            x={0}
+            y={0}
+          />
+          {currentPoints.map((p) => {
+            if (mode === MODES.EDIT) {
+              return <EditablePoint key={p.id} point={p} onMove={movePoint} />;
+            }
 
-        {currentPoints.map((p) => {
-          if (mode === MODES.EDIT) {
-            return <EditablePoint key={p.id} point={p} onMove={movePoint} />;
-          }
-
-          if (mode === MODES.DELETE) {
-            return (
-              <DeletablePoint key={p.id} point={p} onDelete={deletePoint} />
-            );
-          }
-
-          return <Point key={p.id} x={p.x} y={p.y} />;
-        })}
-      </Mafs>
-    </>
+            if (mode === MODES.DELETE) {
+              return (
+                <DeletablePoint key={p.id} point={p} onDelete={deletePoint} />
+              );
+            }
+            return <Point color={colorPoints} key={p.id} x={p.x} y={p.y} />;
+          })}
+          <style type="text/css">
+            {`.MafsView{` +
+              "background:" +
+              background +
+              ";" +
+              "--mafs-line-color:" +
+              colorLine +
+              ";" +
+              "--grid-line-subdivision-color:" +
+              colorLine +
+              ";" +
+              `}`}
+          </style>
+        </Mafs>
+      </div>
+    </div>
   );
 }
