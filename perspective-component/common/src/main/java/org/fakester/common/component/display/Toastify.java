93f30682bf9fa3f6521e55a468efd507b1e920b4
Jonathan Zapata Ponciano <JONZAPA@tbdir.net>
2026-02-16 09:58:06 -0600

    Add Toastify component schema and properties; remove unused event schema and icon

--- perspective-component/common/src/main/java/org/fakester/common/component/display/Toastify.java
+++ perspective-component/common/src/main/java/org/fakester/common/component/display/Toastify.java
@@ -1,14 +1,11 @@
 package org.fakester.common.component.display;
 
-import java.util.List;
 
 import org.fakester.common.RadComponents;
 
 import com.inductiveautomation.ignition.common.jsonschema.JsonSchema;
 import com.inductiveautomation.perspective.common.api.ComponentDescriptor;
 import com.inductiveautomation.perspective.common.api.ComponentDescriptorImpl;
-import com.inductiveautomation.perspective.common.api.ComponentEventDescriptor;
-
 
 /**
  * Common meta information about the Toastify component. See {@link Image} for docs on each field.
@@ -19,9 +16,6 @@ public class Toastify {
     public static final JsonSchema SCHEMA =
         JsonSchema.parse(RadComponents.class.getResourceAsStream("/toastify.props.json"));
 
-    public static final JsonSchema POINT_EVENT_SCHEMA =
-        JsonSchema.parse(RadComponents.class.getResourceAsStream("/toastify.event.props.json"));
-        
     public static ComponentDescriptor DESCRIPTOR = ComponentDescriptorImpl.ComponentBuilder.newBuilder()
         .setPaletteCategory(RadComponents.COMPONENT_CATEGORY)
         .setId(COMPONENT_ID)
@@ -30,23 +24,6 @@ public class Toastify {
         .setName("Toastify")
         .setDefaultMetaName("toastify")
         .addPaletteEntry("", "Toastify", "A component that uses component messaging and data fetching delegates.", null, null)
-        .setEvents(List.of(
-            new ComponentEventDescriptor(
-                "onPointCreated",
-                "Fires after a user adds a point to the plane.",
-                POINT_EVENT_SCHEMA
-            ),
-            new ComponentEventDescriptor(
-                "onPointReleased",
-                "Fires after a user drags a point and releases it in edit mode.",
-                POINT_EVENT_SCHEMA
-            ),
-            new ComponentEventDescriptor(
-                "onPointDeleted",
-                "Fires after a user delete a point.",
-                POINT_EVENT_SCHEMA
-            )
-        ))
         .setResources(RadComponents.BROWSER_RESOURCES)
         .build();
 
